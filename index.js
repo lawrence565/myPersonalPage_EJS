ea5315f9b56c3d3dc0025c41e5ac7d7354e22da9
Lawrence <lawrence891106@gmail.com>
2024-07-12 22:33:21 +0800

    adding SQL function

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 import express from "express";
 import bodyParser from "body-parser";
-import nodemailer from "nodemailer";
+import pg from "pg";
 import "dotenv/config";
 
 const app = express();
 const port = 3000;
+const db = new pg.Client({
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  database: "Message",
+  password: process.env.POSTGRES_PASS,
+  port: process.env.POSTGRES_PORT,
+});
 
+db.connect();
 app.use(bodyParser.urlencoded(true));
 app.use(express.static("public"));
 
@@ -27,28 +35,26 @@ app.get("/preview", (req, res) => {
 
 app.post("/message", (req, res) => {
   let message = req.body;
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
 
-  const mailOptions = {
-    from: `${message.email}`,
-    to: process.env.EMAIL_USER,
-    subject: `Message from ${message.name}`,
-    text: `You have a new message from ${message.name}.\nThe message is ${message.message}. \n\nHere is the contact:\nPhone: ${message.tel}\nName: ${message.name}\nEmail: ${message.email}`,
-  };
+  try {
+    db.connect();
+    db.query(
+      "INSERT INTO message (name, phone, email, message) VALUES ($1, $2, $3, $4)",
+      [message.name, message.phone, message.email, message.message],
+      (err, res) => {
+        if (err) {
+          console.error("Error executing query", err.stack);
+        } else {
+          console.log("Store message success.");
+        }
+      }
+    );
+    db.end();
+  } catch (e) {
+    console.log(e);
+  }
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      return res.status(500).send(error.toString());
-    }
-    console.log("Email sent: " + info.response);
-    res.status(200).redirect("/");
-  });
+  res.redirect("/");
 });
 
 app.listen(port, (req, res) => {
